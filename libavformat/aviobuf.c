1f218e4c3089629432b68c46e4625fc28306a6b6
Markus Koschany <apo@debian.org>
2018-12-30 12:12:16 +0100

    avio_feof
    This function is needed for several of the CVE-2017-140xx flaws.
    
    Gbp-Pq: Name avio_feof.patch

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -257,6 +257,17 @@ int64_t avio_size(AVIOContext *s)
     return size;
 }
 
+int avio_feof(AVIOContext *s)
+{
+    if(!s)
+        return 0;
+    if(s->eof_reached){
+        s->eof_reached=0;
+        fill_buffer(s);
+    }
+    return s->eof_reached;
+}
+
 void avio_wl32(AVIOContext *s, unsigned int val)
 {
     avio_w8(s, val);
